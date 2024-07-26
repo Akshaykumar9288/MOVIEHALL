2e62805c5aee92dcc1771a40918a188424ae4f28
Akshay Kumar <akshayk0552@gmail.com>
2024-07-26 12:28:26 +0530

    MOVIEHALL

---
+++ MOVIEHALL/src/main/java/com/MOVIEHALL/MoviehallApplication.java
@@ -0,0 +1,13 @@
+package com.MOVIEHALL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviehallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviehallApplication.class, args);
+	}
+
+}
